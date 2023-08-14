7324945c43b5e4496bb1b9bdc400b1804d6d3083
DavideSegullo <davidesegullo@gmail.com>
2023-08-14 17:10:30 +0200

    feat: :sparkles: add hash example

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Crypto from 'react-native-quick-crypto'
 
 export default function App() {
+  const hashed = Crypto.createHash('sha256')
+  .update('Damn, Margelo writes hella good software!')
+  .digest('hex');
+
+  console.log(hashed);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
